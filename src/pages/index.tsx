ea3e136b07bde7d0f65045f60354cef68a413efe
So-Emily <esoriano110@gmail.com>
2024-12-16 17:51:07 -0800

    fixed home and login setup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import Link from 'next/link';
+// import Link from 'next/link';
 import Navbar from '@/components/Navbar/Navbar';
 
 const HomePage = () => {
@@ -6,15 +6,16 @@ const HomePage = () => {
         <div>
             <Navbar />
             <main className="flex flex-col items-center justify-center min-h-screen bg-gray-900 text-white">
-                <h1 className="text-4xl font-bold mb-4">Welcome to the QR Code <br/><br/> Buddy Booze Application <br/></h1>
-                <p className="text-lg mb-6">Please sign in to continue.</p>
-                
-                
+
+                <h1 className="text-4xl font-bold mb-20">Welcome to the QR Code <br/><br/> Buddy Booze Application</h1>
+
+                {/* Login Title and Button */}
+                {/* <p className="text-lg mb-6 mt-20">Please sign in to continue</p>
                 <Link href="/auth/signin" passHref>
                     <button className="px-6 py-3 bg-blue-500 text-white rounded hover:bg-blue-600">
                         Go to Login Page
                     </button>
-                </Link>
+                </Link> */}
             </main>
         </div>
     );
